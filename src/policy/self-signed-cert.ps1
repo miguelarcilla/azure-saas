c04c26b86d7db1bacc9bd22646441ebb63f46189
LaBrina Loving <labrina.loving@gmail.com>
2022-04-29 10:01:14 -0400

    Adding custom policies

---
+++ src/policy/self-signed-cert.ps1
@@ -0,0 +1,7 @@
+New-SelfSignedCertificate -DnsName "root_ca_dev_damienbod.com", "root_ca_dev_damienbod.com" -CertStoreLocation "cert:\LocalMachine\My" -NotAfter (Get-Date).AddYears(20) -FriendlyName "root_ca_dev_damienbod.com" -KeyUsageProperty All -KeyUsage CertSign, CRLSign, DigitalSignature
+
+$mypwd = ConvertTo-SecureString -String "1234" -Force -AsPlainText
+
+Get-ChildItem -Path cert:\localMachine\my\ | Export-PfxCertificate -FilePath C:\git\root_ca_dev_damienbod.pfx -Password $mypwd
+
+Export-Certificate -Cert cert:\localMachine\my\ -FilePath root_ca_dev_damienbod.crt
